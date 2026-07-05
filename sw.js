b05e0e125045c78f963f57fdffa5f0db842851cf
Capelard <zsdcz5w2g6@privaterelay.appleid.com>
2026-07-05 09:27:58 -0500

    v2.3: rex accessory anchors corrected (glasses on the googly eyes, not above them); boss inherits

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const C='dino-survival-v24';
+const C='dino-survival-v25';
 const CORE=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>{
   e.waitUntil(caches.open(C).then(c=>c.addAll(CORE)));
