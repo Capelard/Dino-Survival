8b4a784a23a1581137d25c4f66d1cb7ec4691232
Capelard <zsdcz5w2g6@privaterelay.appleid.com>
2026-07-05 09:00:32 -0500

    v2.0: Angry Birds style level map - winding dotted path, star nodes, boss nodes, dino marker hops to next level

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const C='dino-survival-v21';
+const C='dino-survival-v22';
 const CORE=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>{
   e.waitUntil(caches.open(C).then(c=>c.addAll(CORE)));
